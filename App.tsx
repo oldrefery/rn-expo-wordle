7f440e6066cc4bd559d9df6b1d8152485b9f259a
Rustem.Shakirov <Rustem.Shakirov@emea.merkleinc.com>
2022-11-09 14:55:35 +0100

    feat: Add the template for displaying attempts and words

--- App.tsx
+++ App.tsx
@@ -1,20 +1,86 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { SafeAreaView, ScrollView, StyleSheet, Text, View } from "react-native";
+import { colors } from "./src/constants";
+import Keyboard from "./src/components/Keyboard";
+
+const MAX_ATTEMPTS = 6;
+type Word = string[];
 
 export default function App() {
+  const word = "world";
+  const letters = word.split("");
+  // const rows = [
+  //   ["a", "p", "p", "l", "e"],
+  //   ["", "", "", "", ""],
+  //   ["", "", "", "", ""],
+  //   ["", "", "", "", ""],
+  //   ["", "", "", "", ""],
+  //   ["", "", "", "", ""],
+  // ];
+  const rows: Word[] = new Array(MAX_ATTEMPTS).fill(
+    new Array(word.length).fill("a")
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Hello world!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar style="light" />
+      <Text style={styles.title}>WORDLE</Text>
+      {rows.map((row, indexRow) => {
+        console.log("indexRow", indexRow, row);
+        return (
+          <View style={styles.map} key={indexRow}>
+            <View style={styles.row}>
+              {row.map((cell, indexCell) => (
+                <View
+                  style={styles.cell}
+                  key={`${cell}-${indexRow}${indexCell}`}
+                >
+                  <Text style={styles.cellText}>{cell}</Text>
+                </View>
+              ))}
+            </View>
+          </View>
+        );
+      })}
+      <Keyboard />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: colors.black,
+    alignItems: "center",
+  },
+  title: {
+    color: colors.lightgrey,
+    fontWeight: "bold",
+    fontSize: 32,
+    letterSpacing: 6,
+  },
+  map: {
+    alignSelf: "stretch",
+  },
+  row: {
+    flexDirection: "row",
+    alignSelf: "stretch",
+    justifyContent: "center",
+  },
+  cell: {
+    borderColor: colors.darkgrey,
+    borderWidth: 3,
+    flex: 1,
+    maxWidth: 70,
+    aspectRatio: 1,
+    margin: 3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  cellText: {
+    textTransform: "uppercase",
+    color: colors.lightgrey,
+    fontSize: 28,
+    fontWeight: "bold",
   },
 });
